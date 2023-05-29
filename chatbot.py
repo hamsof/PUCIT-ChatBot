d33d2aa8de90c9fbfb039b76339d2b0e8b0949c5
hamsof <hafizabdulman@gmail.com>
2023-05-29 20:00:13 +0500

    adding model to flask

---
+++ chatbot.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, render_template
+import pickle
+import json 
+import numpy as np
+from tensorflow import keras
+
+
+app = Flask(__name__)
+
+# Load the pickled function
+
+
+
+with open("intent.json") as file:
+    data = json.load(file)
+
+
+def chat(input):
+    # load trained model
+    model = keras.models.load_model('chat_model')
+    # load tokenizer object
+    with open('tokenizer.pickle', 'rb') as handle:
+        tokenizer = pickle.load(handle)
+
+    # load label encoder object
+    with open('label_encoder.pickle', 'rb') as enc:
+        lbl_encoder = pickle.load(enc)
+
+    # parameters
+    max_len = 20
+    result = model.predict(keras.preprocessing.sequence.pad_sequences(tokenizer.texts_to_sequences([input]),
+                                            truncating='post', maxlen=max_len))
+    tag = lbl_encoder.inverse_transform([np.argmax(result)])
+
+    for i in data['intents']:
+        if i['tag'] == tag:
+            return np.random.choice(i['responses'])
+
+
+
+
+
+# Route for the home page
+@app.route('/')
+def hello_world():
+    return render_template('index.html')
+
+# Route for prediction
+@app.route('/predict')
+def predict(input):
+    #message = request.form['message']
+    output = chat(input)
+    return output
+
+if __name__ == '__main__':
+    app.run()
--- chatbot_Serializer/py.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-def function_serializer(text_input):
-    return "You got me it`s HAMSOF"
-
-#serialization of this function to a file
-with open('py.pkl', 'wb') as file:
-    pickle.dump(function_serializer, file)
